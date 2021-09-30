38186d5cf960e2ec10d71afd23f2fe2c2e365473
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2021-09-30 08:55:50 -0500

    Set some base styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Movie Search</h1>
+        </div>
+    );
 }
 
 export default App;
