6dc115319e29eff54ed5b8eaba08cfd914417d74
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2021-09-30 09:55:08 -0500

    Function that fetches the movies

--- src/components/SearchMovies/index.js
+++ src/components/SearchMovies/index.js
@@ -2,8 +2,28 @@ import React from "react";
 import "./SearchMovies.css";
 
 function MovieSearch(props) {
+    const handleSubmit = function (e) {
+        e.preventDefault();
+
+        let query = "jurassic park";
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=b69e29c842da7668bd25f26c30313183&language=en-US&query=${query}&page=1&include_adult=false`;
+
+        fetch(url)
+            .then(function (response) {
+                if (response.ok) return response.json();
+                throw response;
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    };
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <label htmlFor="query" className="label">
                 Movie Name
             </label>
